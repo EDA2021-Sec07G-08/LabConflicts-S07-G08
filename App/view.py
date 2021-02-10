b6900654cb59febf0e6ce14fdbee93b48dfdd2f5
felipenunezpinillos <felipenunezpinillos@gmail.com>
2021-02-10 18:18:15 -0500

    finalizacion de merge y control de cambios

--- App/view.py
+++ App/view.py
@@ -43,11 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TO-DO: Modificación de Est-1 en el Lab 2
-<<<<<<< HEAD
     print("3-Cargar Book Tags!")
-=======
-    print("3-Cargar Book Tags")
->>>>>>> origin/Alejandro-Segura---2
     print("0- Salir")
 
 
